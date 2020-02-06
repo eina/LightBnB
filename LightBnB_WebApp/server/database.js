1464aadd69ac7aae4d8adc6c0906faf53c455662
Eina Onting <einers@outlook.com>
2020-02-05 19:34:04 -0800

    write connect queries for getUserWithId, getUserWithEmail, and addUser

---
+++ LightBnB_WebApp/server/database.js
@@ -0,0 +1,107 @@
+const properties = require("./json/properties.json");
+const users = require("./json/users.json");
+
+const { Pool } = require("pg");
+
+const config = {
+  user: "vagrant",
+  host: "localhost",
+  database: "lightbnb",
+  password: "123"
+};
+
+const pool = new Pool(config);
+pool.connect();
+
+/// Users
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function(email) {
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then(res => {
+      if (res.rows && res.rows.length) {
+        return res.rows[0];
+      } else {
+        return Promise.resolve(null);
+      }
+    })
+    .catch(err => console.log("query error", err));
+};
+exports.getUserWithEmail = getUserWithEmail;
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function(id) {
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then(res => res.rows)
+    .catch(err => console.log("query error", err));
+  // return Promise.resolve(users[id]);
+};
+exports.getUserWithId = getUserWithId;
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser = function(user) {
+  const { name, email, password } = user;
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [
+      name,
+      email,
+      password
+    ])
+    .then(res => res.rows[0].id)
+    .catch(err => console.error("query error", err));
+};
+exports.addUser = addUser;
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function(guest_id, limit = 10) {
+  return getAllProperties(null, 2);
+};
+exports.getAllReservations = getAllReservations;
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = function(options, limit = 10) {
+  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit]).then(res => {
+    return res.rows;
+  });
+};
+exports.getAllProperties = getAllProperties;
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function(property) {
+  const propertyId = Object.keys(properties).length + 1;
+  property.id = propertyId;
+  properties[propertyId] = property;
+  return Promise.resolve(property);
+};
+exports.addProperty = addProperty;
--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,6 +6,10 @@ DROP TABLE IF EXISTS reservations
 CASCADE;
 DROP TABLE IF EXISTS property_reviews
 CASCADE;
+DROP TABLE IF EXISTS rates
+CASCADE;
+DROP TABLE IF EXISTS guest_reviews
+CASCADE;
 
 CREATE TABLE users
 (
@@ -51,6 +55,25 @@ CREATE TABLE property_reviews
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
+
+CREATE TABLE rates
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+CREATE TABLE guest_reviews
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
