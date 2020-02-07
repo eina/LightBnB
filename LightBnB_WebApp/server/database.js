34e1692022c0f5b7962b4d320edcf8837c361cfa
Eina Onting <einers@outlook.com>
2020-02-07 01:42:32 +0000

    add new property

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -118,7 +118,7 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
 
-  if (!(owner_id || minimum_price_per_night || maximum_price_per_night, minimum_rating)) {
+  if (!(city || owner_id || minimum_price_per_night || maximum_price_per_night, minimum_rating)) {
     queryString += `WHERE `;
   } else {
     queryString += `AND `;
@@ -168,9 +168,10 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryString = `INSERT INTO properties
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)  RETURNING *;`;
+  let queryParams = Object.values(property);
+
+  return pool.query(queryString, queryParams).then(res => res.rows);
 };
 exports.addProperty = addProperty;
